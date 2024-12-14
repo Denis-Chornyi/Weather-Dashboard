66ba5d93974a45de0a2656e81d1aa2a8c1d9a515
Denis Chornyi <chornyi.den@gmail.com>
2024-12-14 17:41:13 +0200

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,23 @@
+// import { ApplicationConfig } from '@angular/core';
+// import { provideHttpClient } from '@angular/common/http';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideHttpClient(),
+//     provideAnimations()
+//   ]
+// };
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { routes } from './routes'; // Імпортуємо маршрути з routes.ts
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideRouter(routes, withComponentInputBinding()) // Додаємо маршрути
   ]
 };
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
-import { WeatherDashboardComponent } from './components/weather-dashboard/weather-dashboard.component';
+import { Routes } from "@angular/router";
+import { WeatherDashboardComponent } from "./components/weather-dashboard/weather-dashboard.component";
 
 export const routes: Routes = [
-  { path: '', component: WeatherDashboardComponent }, 
-  { path: '**', redirectTo: '' }, 
+  { path: "", component: WeatherDashboardComponent },
+  { path: "**", redirectTo: "" },
 ];
